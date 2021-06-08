a49d3e7677cbe94f2b4a966869be75f74fdce179
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-06-08 10:09:38 -0500

    UniqueCharactersHashSet

--- src/dataStructureImplementation/StringHashingArrayPrograms.java
+++ src/dataStructureImplementation/StringHashingArrayPrograms.java
@@ -1,13 +1,17 @@
 package dataStructureImplementation;
 
 import java.util.HashSet;
+import java.util.Hashtable;
 
 public class StringHashingArrayPrograms {
 
     public static void main(String args[]) {
         StringHashingArrayPrograms sha = new StringHashingArrayPrograms();
-        //System.out.println(sha.UniqueCharactersArray("Hello"));
+        /*System.out.println(sha.UniqueCharactersArray("Hello"));
         System.out.println(sha.UniqueCharactersHashSet("Vampire"));
+        sha.practise1("viiimmal");
+        sha.practise2();*/
+        System.out.println(sha.UniqueCharactersHashTable("Worldd"));
     }
 
     //Time - O(n^2)
@@ -25,7 +29,7 @@ public class StringHashingArrayPrograms {
 
     public boolean UniqueCharactersHashSet(String s) {
         int size = s.length();
-        HashSet<Character> set = new HashSet<Character>(s.length());
+        HashSet<Character> set = new HashSet<Character>();
         for(int i=0; i<size; i++) {
             if(set.contains(s.charAt(i))) {
                 return false;
@@ -34,4 +38,43 @@ public class StringHashingArrayPrograms {
         }
         return true;
     }
+
+    public boolean UniqueCharactersHashTable(String s) {
+        int size = s.length();
+        Hashtable<Character,Integer> table = new Hashtable<Character,Integer>();
+        for(int i=0; i<size; i++) {
+            int count =0;
+            if(table.contains(s.charAt(i))) {
+                System.out.println("Contains " + table);
+                return false;
+            }
+
+            table.put(s.charAt(i), count=count+1);
+            System.out.println("Put " + table);
+        }
+        return true;
+    }
+
+    //Hashset values adding
+    public void practise1(String s) {
+        int size = s.length();
+        HashSet<Character> set = new HashSet<>();
+        for (int i=0; i<size; i++) {
+            set.add(s.charAt(i));
+            System.out.println(set);
+        }
+        System.out.println("Set " + set);
+    }
+
+    //HashTable values adding
+    public void practise2() {
+        Hashtable<Integer, String> tab = new Hashtable<>();
+            tab.put(1, "Vimal");
+            tab.put(1, "Vijayalakshmi");
+            tab.put(2, "Vimal");
+            tab.put(2, "Arun");
+            tab.put(3, "Priya");
+            tab.put(2, "Priya");
+        System.out.println("Table " + tab);
+        }
 }
