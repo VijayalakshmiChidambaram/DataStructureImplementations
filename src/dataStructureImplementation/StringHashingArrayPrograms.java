82d38058b2c751305379d73a2326faec1e4c3bc0
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-06-08 12:11:01 -0500

    UniqueCharactersHashTable

--- src/dataStructureImplementation/StringHashingArrayPrograms.java
+++ src/dataStructureImplementation/StringHashingArrayPrograms.java
@@ -7,11 +7,11 @@ public class StringHashingArrayPrograms {
 
     public static void main(String args[]) {
         StringHashingArrayPrograms sha = new StringHashingArrayPrograms();
-        /*System.out.println(sha.UniqueCharactersArray("Hello"));
-        System.out.println(sha.UniqueCharactersHashSet("Vampire"));
-        sha.practise1("viiimmal");
-        sha.practise2();*/
-        System.out.println(sha.UniqueCharactersHashTable("Worldd"));
+        /*sha.practise1("viiimmal");
+        sha.practise2();
+        System.out.println(sha.UniqueCharactersArray("Hello"));*/
+        System.out.println(sha.UniqueCharactersHashSet("Vvampire"));
+        System.out.println(sha.UniqueCharactersHashTable("Wworld"));
     }
 
     //Time - O(n^2)
@@ -43,13 +43,12 @@ public class StringHashingArrayPrograms {
         int size = s.length();
         Hashtable<Character,Integer> table = new Hashtable<Character,Integer>();
         for(int i=0; i<size; i++) {
-            int count =0;
-            if(table.contains(s.charAt(i))) {
-                System.out.println("Contains " + table);
-                return false;
+            int count=0;
+            if(table.containsKey(s.charAt(i))) {
+                    System.out.println("Contains " + table);
+                    return false;
             }
-
-            table.put(s.charAt(i), count=count+1);
+            table.put(s.charAt(i),count+1);
             System.out.println("Put " + table);
         }
         return true;
@@ -72,9 +71,9 @@ public class StringHashingArrayPrograms {
             tab.put(1, "Vimal");
             tab.put(1, "Vijayalakshmi");
             tab.put(2, "Vimal");
-            tab.put(2, "Arun");
-            tab.put(3, "Priya");
-            tab.put(2, "Priya");
+            tab.put(2, "A");
+            tab.put(3, "P");
+            tab.put(2, "P");
         System.out.println("Table " + tab);
         }
 }
