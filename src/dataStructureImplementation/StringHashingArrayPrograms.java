f960e07518d1cc99a5c720a70038a49a7af05aa2
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-06-07 22:53:21 -0500

    UniqueCharactersHashSet

--- src/dataStructureImplementation/StringHashingArrayPrograms.java
+++ src/dataStructureImplementation/StringHashingArrayPrograms.java
@@ -1,10 +1,13 @@
 package dataStructureImplementation;
 
+import java.util.HashSet;
+
 public class StringHashingArrayPrograms {
 
     public static void main(String args[]) {
         StringHashingArrayPrograms sha = new StringHashingArrayPrograms();
-        System.out.println(sha.UniqueCharactersArray("Hello"));
+        //System.out.println(sha.UniqueCharactersArray("Hello"));
+        System.out.println(sha.UniqueCharactersHashSet("Vampire"));
     }
 
     //Time - O(n^2)
@@ -20,5 +23,15 @@ public class StringHashingArrayPrograms {
         return true;
     }
 
-
+    public boolean UniqueCharactersHashSet(String s) {
+        int size = s.length();
+        HashSet<Character> set = new HashSet<Character>(s.length());
+        for(int i=0; i<size; i++) {
+            if(set.contains(s.charAt(i))) {
+                return false;
+            }
+            set.add(s.charAt(i));
+        }
+        return true;
+    }
 }
