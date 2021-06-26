7317a0c3fa04ef101968f23dc9359c69a8e89f68
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-06-26 17:29:17 -0500

    Creating list of nodes - Singly LL

--- src/dataStructureImplementation/LinkedListDataStructure.java
+++ src/dataStructureImplementation/LinkedListDataStructure.java
@@ -2,12 +2,16 @@ package dataStructureImplementation;
 
 public class LinkedListDataStructure {
     public static void main(String[] args) {
-        Node node = new Node(1);
-
-        System.out.println(node);
+        singlyLinkedList node = new singlyLinkedList();
+        node.createNodes(11);
+        node.createNodes(2);
+        node.createNodes(1);
+        node.createNodes(20);
+        node.printValues();
     }
-
 }
+
+class singlyLinkedList {
     //Define own datatype
     class Node {
         int data;
@@ -17,3 +21,25 @@ public class LinkedListDataStructure {
             data = dataValue;
         }
     }
+        Node head;
+        Node temp;
+
+        public void createNodes(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = temp = newNode;
+            } else {
+                temp.next = newNode;
+                temp = newNode;
+            }
+        }
+
+        public void printValues() {
+            Node currentNode = head;
+            System.out.println("LL");
+            while (currentNode != null) {
+                System.out.println(currentNode.data);
+                currentNode = currentNode.next;
+            }
+        }
+}
