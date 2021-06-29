9f66aac45744b8ee7b6db6b3edae24723863b0ef
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-06-29 11:38:36 -0500

    removeDuplicatesUsingHashset

--- src/dataStructureImplementation/LLProblemSolving.java
+++ src/dataStructureImplementation/LLProblemSolving.java
@@ -1,13 +1,20 @@
 package dataStructureImplementation;
 
+import java.util.HashSet;
+import java.util.Hashtable;
+
 public class LLProblemSolving {
     public static void main(String[] args) {
         singlyLinkedListPrograms llist = new singlyLinkedListPrograms();
         llist.createNodes(1);
         llist.createNodes(10);
+        llist.createNodes(10);
+        llist.createNodes(30);
         llist.createNodes(20);
         llist.createNodes(30);
 
+        llist.printNodes();
+        llist.removeDuplicatesUsingHashset();
         llist.printNodes();
     }
 }
@@ -38,10 +45,48 @@ class singlyLinkedListPrograms {
     public void printNodes() {
         Nodes currentNode;
         currentNode = head;
+        System.out.println("LL Data: ");
         while (currentNode!=null) {
             System.out.println(currentNode.data);
             currentNode = currentNode.next;
         }
+    }
+//1) Removing duplicates using extra space - Hashset, Time complexity - O(n)
+    public void removeDuplicatesUsingHashset() {
+     Nodes prev, curr;
+     prev=head;
+     curr=head;
+     HashSet<Integer> set = new HashSet<>();
+
+     while(curr != null) {
+         if(set.contains(curr.data)) {
+             prev.next = curr.next;
+         }
+         else {
+             set.add(curr.data);
+             prev=curr;
+         }
+         curr=curr.next;
+     }
+    }
+//1) Removing duplicates using extra space - HashTable, Time complexity - O(n)
+    public void removeDuplicatesHashTable() {
+        Nodes curr, prev;
+        curr = head;
+        prev = null;
+        Hashtable<Integer, Boolean> table = new Hashtable<>();
+
+        while (curr !=null) {
+            if (table.containsKey(curr.data)) {
+                prev.next = curr.next;
+            }
+            else{
+                table.put(curr.data, true);
+                prev = curr;
+            }
+            curr = curr.next;
+        }
+
     }
 }
 
