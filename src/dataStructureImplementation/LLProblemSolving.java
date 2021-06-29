89539e2ebc362a4d5a2a39a84d2e65d5c992ccc3
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-06-28 22:36:50 -0500

    Initialising node

--- src/dataStructureImplementation/LLProblemSolving.java
+++ src/dataStructureImplementation/LLProblemSolving.java
@@ -1,4 +1,21 @@
 package dataStructureImplementation;
 
 public class LLProblemSolving {
+    public static void main(String[] args) {
+        LLProblemSolving llist= new LLProblemSolving();
+    }
 }
+
+class singlyLinkedListPrograms {
+    class Nodes {
+        int data;
+        Nodes next;
+
+        public Nodes(int data) {
+            this.data = data;
+        }
+    }
+
+}
+
+
