f746f2c26af6eec93e06ec2e522784a7125275a9
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-07-20 10:51:47 -0500

    popStack

--- src/dataStructureImplementation/StackDataStructure.java
+++ src/dataStructureImplementation/StackDataStructure.java
@@ -8,16 +8,26 @@ public class StackDataStructure {
 
         stackds.pushStack(1);
         stackds.pushStack(2);
+        stackds.pushStack(3);
+        stackds.popStack();
     }
 
     int top = -1;
     int arraySize = 5;
-    int[] push = new int[arraySize];
+    int[] stackElement = new int[arraySize];
     public void pushStack(int element) {
         top++;
         if (top >= arraySize-1) {
             System.out.println("Overflow Condition");
         }
-        push[top] = element;
+        stackElement[top] = element;
+    }
+    public int popStack() {
+        int item = stackElement[top];
+        top--;
+        if(top == -1) {
+            System.out.println("Underflow confition");
+        }
+        return item;
     }
 }
