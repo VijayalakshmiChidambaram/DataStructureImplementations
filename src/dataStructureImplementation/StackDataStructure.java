99efa2cb39e1f50c10a8a945c2b7fcf3e9e2f3fe
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-07-20 10:16:12 -0500

    StackDataStructure

---
+++ src/dataStructureImplementation/StackDataStructure.java
@@ -0,0 +1,20 @@
+package dataStructureImplementation;
+
+import java.util.Scanner;
+
+public class StackDataStructure {
+    public static void main(String[] args) {
+        StackDataStructure stackds = new StackDataStructure();
+
+        System.out.println("Enter the array size");
+        Scanner newscanner = new Scanner(System.in);
+        int arraySize = newscanner.nextInt();
+
+        stackds.push(1);
+        stackds.push(2);
+    }
+    int top = -1;
+    public void push(int element) {
+
+    }
+}
