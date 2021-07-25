0379277085218125c3c8de5d91d5b898711e2878
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-07-24 23:23:40 -0500

    queueds

---
+++ src/dataStructureImplementation/QueueDataStructure.java
@@ -0,0 +1,32 @@
+package dataStructureImplementation;
+
+import java.util.EmptyStackException;
+
+public class QueueDataStructure<size> {
+    public static void main(String[] args) {
+        QueueDataStructure queueds = new QueueDataStructure();
+        queueds.enqueueArray(5);
+        queueds.enqueueArray(3);
+        queueds.enqueueArray(15);
+        queueds.enqueueArray(13);
+        queueds.enqueueArray(55);
+    }
+    int size = 4;
+    int enqueueArr[] = new int[size];
+    int front =-1, rear = -1;
+
+    public void enqueueArray(int value) {
+        if(rear == size-1) {
+            throw new EmptyStackException();
+        }
+        else if(front == -1 && rear == -1) {
+            front = rear = 0;
+            enqueueArr[rear] = value;
+        }
+        else {
+            rear++;
+            enqueueArr[rear] = value;
+        }
+
+    }
+}
