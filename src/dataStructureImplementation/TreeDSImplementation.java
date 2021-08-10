bbb4b149f75af24c6127fa3e2972ae69ea68e0e4
VijayalakshmiChidambaram <vijayalakshmi1994.kc@gmail.com>
2021-08-09 21:19:17 -0500

    binarySearchTreeInsertion

--- src/dataStructureImplementation/TreeDSImplementation.java
+++ src/dataStructureImplementation/TreeDSImplementation.java
@@ -7,15 +7,22 @@ import java.util.Scanner;
 public class TreeDSImplementation {
     public static void main(String[] args) {
         binarayTreeDS treeDS = new binarayTreeDS();
-        binarayTreeDS.TreeNode root = treeDS.createTreeNode(4);
+        /*binarayTreeDS.TreeNode root = treeDS.createTreeNode(4);
         root.left = treeDS.createTreeNode(5);
         root.right = treeDS.createTreeNode(10);
         root.left.left = treeDS.createTreeNode(7);
         root.left.right = treeDS.createTreeNode(8);
-        root.right.right = treeDS.createTreeNode(1);
+        root.right.right = treeDS.createTreeNode(1);*/
         //treeDS.preorder(root);
         //treeDS.inorderTraversal(root);
-        treeDS.postOrderTraversal(root);
+        //treeDS.postOrderTraversal(root);
+        treeDS.binarySearchTreeInsertion(8);
+        treeDS.binarySearchTreeInsertion(5);
+        treeDS.binarySearchTreeInsertion(10);
+        treeDS.binarySearchTreeInsertion(7);
+        treeDS.binarySearchTreeInsertion(17);
+        treeDS.binarySearchTreeInsertion(1);
+        treeDS.binarySearchTreeInsertion(4);
     }
 
 }
@@ -37,7 +44,7 @@ class binarayTreeDS {
     }
 
     public void preorder(TreeNode root) {
-        if(root == null) {
+        if (root == null) {
             return;
         }
         System.out.println(root.data);
@@ -46,7 +53,7 @@ class binarayTreeDS {
     }
 
     public void inorderTraversal(TreeNode root) {
-        if(root == null) {
+        if (root == null) {
             return;
         }
         inorderTraversal(root.left);
@@ -55,11 +62,44 @@ class binarayTreeDS {
     }
 
     public void postOrderTraversal(TreeNode root) {
-        if(root == null) {
+        if (root == null) {
             return;
         }
         postOrderTraversal(root.left);
         postOrderTraversal(root.right);
         System.out.println(root.data);
     }
+
+    TreeNode root;
+
+    public TreeNode binarySearchTreeInsertion(int data) {
+        TreeNode newNode = createTreeNode(data);
+        if (root == null) {
+            root = newNode;
+        } else {
+            TreeNode newroot = root;
+            while(newNode!=null) {
+            if (newNode.data < newroot.data) {
+                if (newroot.left == null) {
+                    newroot.left = newNode;
+                    break;
+                } else {
+                    newroot = newroot.left;
+                }
+            }
+            else if(newNode.data > newroot.data) {
+                if(newroot.right == null) {
+                    newroot.right = newNode;
+                    break;
+                }
+                else {
+                    newroot = newroot.right;
+                }
+            }
+
+            }
+        }
+        return root;
+    }
+
 }
